4a8f7cca3addd5a709fbf9a3133be8225d0aa701
Pierrick Delage <pierrick.delage@gmail.com>
2020-02-19 09:31:21 +0100

    ajout btn achat

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'plugins/shuffle.min';
 
 import "bootstrap";
 
@@ -9,114 +8,7 @@ import "bootstrap";
 //= require jquery_ujs
 
 import { loadDynamicBannerText } from '../components/banner';
-loadDynamicBannerText();
 
-
-(function ($) {
-    'use strict';
-    
-AOS.init({
-    once: true
-});
-
-    // ----------------------- 
-        // Progress Bar--------------------
-        // 
-        // 
-
-    $(window).on ('load', function (){ 
-          
-        $('.progress-bar').each(function(){
-                var width = $(this).data('percent');
-                $(this).css({'transition': 'width 3s'});
-                $(this).appear(function() {
-                    console.log('hello');
-                    $(this).css('width', width + '%');
-                    $(this).find('.count').countTo({
-                        from: 0,
-                        to: width,
-                        speed: 3000,
-                        refreshInterval: 50
-                    });
-                });
-            });
-        }); 
-
-    $('.owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        dots:false,
-        autoplayTimeout:8000
-    });
-
-    // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
-
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
-
-     $('.portfolio-gallery').each(function () {
-        $(this).find('.popup-gallery').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    });
-
-
-})(jQuery);
-
-
-(function ($) {
-    'use strict';
-
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
-
-    // Success function
-    function done_func(response) {
-        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
-        message.text(response);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-        form.find('input:not([type="submit"]), textarea').val('');
-    }
-
-    // fail function
-    function fail_func(data) {
-        message.fadeIn().removeClass('alert-success').addClass('alert-success');
-        message.text(data.responseText);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-    }
-    
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
-        .done(done_func)
-        .fail(fail_func);
-    });
-    
-})(jQuery);
-
-
+if(document.getElementById("banner-typed-text")){
+    loadDynamicBannerText();
+}
