b47e643c4630cf0a6e6995ead4139d6c457c588c
Pierrick Delage <pierrick.delage@gmail.com>
2020-02-24 13:18:21 +0100

    working on CRUD

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,11 +1,51 @@
 class ShipsController < ApplicationController
-	skip_before_action :authenticate_user!
+	skip_before_action :authenticate_user!, :only => [:index, :show]
+  	before_action :set_ship, only: [:show, :edit, :update, :destroy]
 
 	def index
 	  @ships = Ship.all
 	end
 
 	def show
-	  @ship = Ship.find(params[:id])
 	end
+
+	def new           # GET /ships/new
+		@ship = Ship.new
+	end
+
+	def create        # POST /ships
+		@ship = Ship.new(ship_params)
+		@ship.save     	
+		if @ship.save 
+			redirect_to ship_path(@ship)
+		else
+			render 'new'
+		end 
+	end
+
+	def edit          # GET /ships/:id/edit
+	end
+
+	def update        # PATCH /ships/:id
+	 	@ship.update(ship_params)
+	 	redirect_to ship_path(@ship)
+	end
+
+	def destroy       # DELETE /ships/:id
+	    @ship.destroy
+	    redirect_to ships_path
+	end
+
+	private
+
+	def set_ship
+		@ship = Ship.find(params[:id])
+	end
+
+	def ship_params
+		# *Strong params*: You need to *whitelist* what can be updated by the user
+		# Never trust user data!
+		params.require(:ship).permit(:name, :description, :dimension, :photo_url, :detail, :quantity, :price)
+	end
+
 end
